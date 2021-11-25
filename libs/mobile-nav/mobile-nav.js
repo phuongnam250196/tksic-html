b049a5f54be444eb44fef41ce9b68c6d347db6d5
phuongnam250196 <phuongnam250196@gmail.com>
2021-11-25 18:14:47 +0700

    fix

--- libs/mobile-nav/mobile-nav.js
+++ libs/mobile-nav/mobile-nav.js
@@ -1,51 +1,58 @@
-(function ($) {
-  "use strict";
+(function($) {
+    "use strict";
 
-  // Mobile Navigation
-  if ($('.main-nav').length) {
-    var $mobile_nav = $('.main-nav').clone().prop({
-      class: 'mobile-nav'
-    });
-    $('body').append($mobile_nav);
+    // Mobile Navigation
+    if ($('.main-nav-mobile').length) {
 
-    $('body #header').prepend('<button type="button" class="mobile-nav-toggle"><i class="ion-android-menu"></i></button>');
-    $('body').append('<div class="mobile-nav-overly"></div>');
+        var $mobile_nav = $('.main-nav-mobile').clone().prop({
+            class: 'mobile-nav',
+        });
+        $('body').append($mobile_nav);
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
-      $('#header').addClass('header-scrolled');
-      $("#header").css({ "position": "fixed" });
-      $("#header .ion-android-menu").css({ "position": "fixed", "top": "inherit", "right": "16px"});
-      $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
-      $('.mobile-nav-overly').toggle();
-    });
+        $('body #header').prepend('<button type="button" class="mobile-nav-toggle"><i class="ion-android-menu"></i></button>');
+        $('body').append('<div class="mobile-nav-overly"></div>');
 
-      $(document).on('click', '.mobile-nav-overly', function(e){
-          $('body').toggleClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
-          $('.mobile-nav-overly').toggle();
-      });
+        $(document).on('click', '.mobile-nav-toggle', function(e) {
+            $('#header').addClass('header-scrolled');
+            $("#header").css({ "position": "fixed" });
+            $("#header .ion-android-menu").css({ "position": "fixed", "top": "inherit", "right": "16px" });
+            $('body').toggleClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
+            $('.mobile-nav-overly').toggle();
+        });
+
+        $(document).on('click', '.mobile-nav-overly', function(e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
+            $('.mobile-nav-overly').toggle();
+        });
 
 
 
-      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-      e.preventDefault();
-      $(this).next().slideToggle(200);
-      $(this).parent().toggleClass('active');
-    });
+        $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+            e.preventDefault();
+            console.log(1)
+            $(this).next().slideToggle(200);
+            $(this).parent().toggleClass('active');
+        });
 
-    $(document).click(function(e) {
-      var container = $(".mobile-nav, .mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
-          $('.mobile-nav-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-    $(".mobile-nav, .mobile-nav-toggle").hide();
-  }
+        $(document).click(function(e) {
+            var container = $(".mobile-nav, .mobile-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('ion-android-close ion-android-menu');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+            }
+        });
+
+        $(".mobile-nav > ul > li").click(function(e) {
+          // $(this).find(".submenu").css({ "transition": "max-height 2s ease-in", "max-height": "500px" });
+          $(this).find(".submenu").toggle();
+        })
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+        $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
 
 })(jQuery);
